a215d784e0dcbe29ab860b451b4c6e00cd0b66f8
Nico Weichbrodt <nico@weichbrodt.me>
2018-07-02 19:33:16 +0200

    uptime

--- system.py
+++ system.py
@@ -9,11 +9,11 @@ class System:
 
     def update(self):
         if utils.testmode():
-            up = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            up = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S\n')
         else:
             up = utils.execute('uptime -s')
 
-        self.start = datetime.datetime.strptime(up, '%Y-%m-%d %H:%M:%S')
+        self.start = datetime.datetime.strptime(up, '%Y-%m-%d %H:%M:%S\n')
         self.uptime = datetime.datetime.now() - self.start
 
     def __str__(self):
